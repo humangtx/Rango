8d88ad616bd8a5f1e154e1755e9d72193a18dfdd
Tianxiao Guo <2474529g@student.gla.ac.uk>
2020-02-20 12:03:27 +0000

    20200220

--- rango/urls.py
+++ rango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import path
 from rango import views
 
+
 app_name = 'rango'
 
 
--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict['categories'] = category_list
+    return render(request, 'rango/Index.html', context=context_dict)
 
 # Create your views here.
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    return render(request, 'rango/About.html', context=context_dict)
